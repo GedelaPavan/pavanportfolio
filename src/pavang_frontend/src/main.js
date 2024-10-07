e08d98a59c6089947a2b0bf14a8afe3172c4bff4
root <root@LAPTOP-4P4037V1.localdomain>
2024-10-07 06:06:28 +0000

    pavan

--- src/pavang_frontend/src/main.js
+++ src/pavang_frontend/src/main.js
@@ -1,4 +1,12 @@
-import App from './App';
-import './index.scss';
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
 
-const app = new App();
+    const name = this[0].value;
+    const email = this[1].value;
+    const message = this[2].value;
+
+    alert(`Thank you, ${name}! Your message has been received.`);
+    // Here you can add functionality to send the message to a server or email service.
+    
+    this.reset(); // Reset form fields
+});
